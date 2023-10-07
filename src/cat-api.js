1470f4f76a86b036a857fdb50760d2c014830c2a
Mariia Svinitska <mariashubna@gmail.com>
2023-10-07 19:41:29 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,19 +5,13 @@ axios.defaults.headers.common['x-api-key'] =
 
 let page = 1;
 const BASE_URL = 'https://api.thecatapi.com';
-const API_KEY =
-  'live_1FrID8A3d3SHqNZpm42jloqifB6oP9agX2Mxj1n6cccy5ravdHAJWtea0u7N9FEX';
 const END_POINT = 'v1/breeds';
 
 
 export function fetchBreeds(page = 1) {
 
-  const params = {
-    api_key: API_KEY,
-    page,
-  };
 
-  return axios.get(`${BASE_URL}/${END_POINT}?${params}`).then(resp => {
+  return axios.get(`${BASE_URL}/${END_POINT}`).then(resp => {
     if (resp.status < 200 || resp.status >= 300) {
       throw new Error(`Fetch error with ${resp.status}: ${resp.statusText}`);
     }
@@ -28,11 +22,8 @@ export function fetchBreeds(page = 1) {
 
 export function fetchCatByBreed(breedId) {
       
-    const params = {
-      breed_ids: breedId,
-    };
-  
-    return axios.get(`${BASE_URL}/${END_POINT}?${params}`).then((resp) => {
+
+    return axios.get(`${BASE_URL}/${END_POINT}?${breedId}`).then((resp) => {
       if (resp.status < 200 || resp.status >= 300) {
         throw new Error(`Fetch error with ${resp.status}: ${resp.statusText}`);
       }
